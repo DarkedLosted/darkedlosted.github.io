fb0690123f5d3c5134684fbb868f6e05f4054fb0
LOST <adrinalinrash@mail.ru>
2018-10-15 02:30:25 +0300

    подправил анимацию

--- js/video.js
+++ js/video.js
@@ -150,23 +150,15 @@ function draw(bands, canvas, ctx) {
 (function() {
     initPlayerControls();
 
-    initVideo(
-        document.getElementById('video-1'),
-        'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A8000%2Fstreams%2Fsosed%2Fmaster.m3u8'
-    );
-
-    initVideo(
-        document.getElementById('video-2'),
-        'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A8000%2Fstreams%2Fcat%2Fmaster.m3u8'
-    );
-
-    initVideo(
-        document.getElementById('video-3'),
-        'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A8000%2Fstreams%2Fdog%2Fmaster.m3u8'
-    );
-
-    initVideo(
-        document.getElementById('video-4'),
+    let videoUrls = [
+        'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A8000%2Fstreams%2Fsosed%2Fmaster.m3u8',
+        'http://localhost.54:9191/master?url=http%3A%2F%2F192.168.1.54%3A8000%2Fstreams%2Fcat%2Fmaster.m3u8',
+        'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A8000%2Fstreams%2Fdog%2Fmaster.m3u8',
         'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A8000%2Fstreams%2Fhall%2Fmaster.m3u8'
-    );
+    ],
+        videoNodes = Array.from(document.getElementsByClassName('video'));
+
+    videoNodes.forEach((video, i) => {
+        initVideo(video, videoUrls[i])
+    });
 })();
