972c948e4aba2cd9352983a488a26d7558d7473a
LOST <adrinalinrash@mail.ru>
2018-10-15 02:31:28 +0300

    исправил ссылку src для потока

--- js/video.js
+++ js/video.js
@@ -152,7 +152,7 @@ function draw(bands, canvas, ctx) {
 
     let videoUrls = [
         'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A8000%2Fstreams%2Fsosed%2Fmaster.m3u8',
-        'http://localhost.54:9191/master?url=http%3A%2F%2F192.168.1.54%3A8000%2Fstreams%2Fcat%2Fmaster.m3u8',
+        'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A8000%2Fstreams%2Fcat%2Fmaster.m3u8',
         'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A8000%2Fstreams%2Fdog%2Fmaster.m3u8',
         'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A8000%2Fstreams%2Fhall%2Fmaster.m3u8'
     ],
